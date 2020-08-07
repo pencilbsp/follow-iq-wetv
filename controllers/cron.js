b485be4e0aea132bd04fd4d899a15184fdfa6c7c
pencilbsp <pencil.bsp@gmail.com>
2020-08-08 02:23:24 +0700

    v1.0

--- controllers/cron.js
+++ controllers/cron.js
@@ -1,14 +1,16 @@
 const CronJob = require('cron').CronJob;
+const request = require('request');
 const low = require('lowdb');
 const FileSync = require('lowdb/adapters/FileSync');
 const adapter = new FileSync('./db.json');
 const db = low(adapter);
+require('dotenv').config()
 
 const urlData = require('./refresh');
 const getEpi = require('./epi');
 
 var job = new CronJob(
-    '*/10 * * * * *',
+    '*/5 * * * * *',
     () => {
         var dataLoop = db.get('follower').map('url').value();
         refreshNewEpi(dataLoop);
@@ -22,13 +24,16 @@ job.start();
 
 function refreshNewEpi(dataLoop) {
     dataLoop.forEach(async (itemLoop) => {
-        let oldEpi = getEpi.full(
-            db.get('follower').find({ url: itemLoop }).value().data
-        );
-        let newEpi = getEpi.full(await urlData.refresh(itemLoop));
+        const oldData = db.get('follower').find({ url: itemLoop }).value();
+        const oldEpi = oldData.data;
+        const name = oldData.title;
+        const newEpi = getEpi.full(await urlData.refresh(itemLoop));
+
         // console.log(oldEpi)
         if (newEpi.length > oldEpi.length) {
-            var diff = [];
+            var diffNew = [];
+            var diffChangeVip = [];
+            var diffChangeNormal = [];
             newEpi.forEach((element) => {
                 if (
                     JSON.stringify(oldEpi).includes(JSON.stringify(element)) ==
@@ -38,25 +43,26 @@ function refreshNewEpi(dataLoop) {
                     if (element.type == 'Vip') {
                         const check = `{"name":"${element.name}","type":"Normal"}`;
                         if (JSON.stringify(oldEpi).includes(check) == true) {
-                            const mess = `${element.name} Normal change to Vip`;
-                            diff.push(mess);
+                            diffChangeNormal.push(element.name);
                         } else {
-                            const mess = `${element.name} added`;
-                            diff.push(mess);
+                            diffNew.push(element.name);
                         }
                     } else {
                         const check = `{"name":"${element.name}","type":"Vip"}`;
                         if (JSON.stringify(oldEpi).includes(check) == true) {
-                            const mess = `${element.name} Vip change to Normal`;
-                            diff.push(mess);
+                            diffChangeVip.push(element.name);
                         } else {
-                            const mess = `${element.name} added`;
-                            diff.push(mess);
+                            diffNew.push(element.name);
                         }
                     }
                 }
             });
-            console.log(diff);
+            const messNew = `[Test] Phim ${name} đã thêm ${diffNew.length} tập mới: ${diffNew.join(', ')}`
+            const messChangeVip = `[Test] ${diffChangeVip.length} tập ${diffChangeVip.join(', ')} phim ${name} đã chuyển Vip sang Normal`
+            const messChangeNormal = `[Test] ${diffChangeNormal.length} tập ${diffChangeNormal.join(', ')} phim ${name} đã chuyển Normal sang Vip`
+            if (diffNew.length > 0) request(`${process.env.TELEGRAM_URL}${encodeURI(messNew)}`);
+            if (diffChangeVip.length > 0) request(`${process.env.TELEGRAM_URL}${encodeURI(messChangeVip)}`);
+            if (diffChangeNormal.length > 0) request(`${process.env.TELEGRAM_URL}${encodeURI(messChangeNormal)}`);
             db.get('follower')
                 .find({ url: itemLoop })
                 .assign({ data: newEpi })
