3d682b38c1b36ae3aa438a6de3302f762cfa0993
pencilbsp <pencil.bsp@gmail.com>
2020-08-07 23:18:08 +0700

    format code

--- controllers/index.js
+++ controllers/index.js
@@ -1,37 +1,48 @@
 const request = require('request');
-const low = require('lowdb')
-const FileSync = require('lowdb/adapters/FileSync')
-const adapter = new FileSync('./db.json')
-const db = low(adapter)
-db.defaults({ follower: [] }).write()
+const low = require('lowdb');
+const FileSync = require('lowdb/adapters/FileSync');
+const adapter = new FileSync('./db.json');
+const db = low(adapter);
+db.defaults({ follower: [] }).write();
 
 module.exports.index = (req, res) => {
     const indexData = db.get('follower').value();
     res.render('index', { title: 'Follow Admin', data: indexData });
-}
+};
 
 module.exports.add = (req, res) => {
-    const url = req.body.url
-    console.log(req.body.time)
+    const url = req.body.url;
+    console.log(req.body.time);
     request(url, async (err, response, body) => {
         try {
             const title = body.match(/<\s*title[^>]*>(.*?)<\s*\/s*title>/)[1];
             const name = title.split('|')[0].trim();
             // Check database
             if (db.get('follower').find({ title: name }).value() == undefined) {
-                let newEpi = await refreshUrlData(url)
+                let newEpi = await refreshUrlData(url);
                 if (newEpi === 'error') {
-                    res.json({ mess: 'Url bạn nhập sai hoặc chưa được hỗ trợ!' })
+                    res.json({
+                        mess: 'Url bạn nhập sai hoặc chưa được hỗ trợ!',
+                    });
                 } else {
-                    db.get('follower').push({ title: name, url: url, data: newEpi, time: req.body.time }).write()
-                    const htmlCode = `<tr><td><span class="custom-checkbox"><input id="checkbox1" type="checkbox" name="options[]" value="1"><label for="checkbox1"></label></span></td><td>${name}</td><td>${url}</td><td>${newEpi.length}</td><td>${req.body.time}</td><td><a class="edit" href="#editEmployeeModal" data-tl="${name}" data-url="${url}" data-toggle="modal"><i class="material-icons" data-toggle="tooltip" title="Edit"></i></a><a class="delete" href="#deleteEmployeeModal" data-tl="${name}" data-toggle="modal"><i class="material-icons" data-toggle="tooltip" title="Delete"></i></a></td></tr>`
+                    db.get('follower')
+                        .push({
+                            title: name,
+                            url: url,
+                            data: newEpi,
+                            time: req.body.time,
+                        })
+                        .write();
+                    const htmlCode = `<tr><td><span class="custom-checkbox"><input id="checkbox1" type="checkbox" name="options[]" value="1"><label for="checkbox1"></label></span></td><td>${name}</td><td>${url}</td><td>${newEpi.length}</td><td>${req.body.time}</td><td><a class="edit" href="#editEmployeeModal" data-tl="${name}" data-url="${url}" data-toggle="modal"><i class="material-icons" data-toggle="tooltip" title="Edit"></i></a><a class="delete" href="#deleteEmployeeModal" data-tl="${name}" data-toggle="modal"><i class="material-icons" data-toggle="tooltip" title="Delete"></i></a></td></tr>`;
                     res.json({
                         mess: `Phim ${name} đã được thêm vào danh sách theo dõi!`,
-                        html: htmlCode
-                    })
+                        html: htmlCode,
+                    });
                 }
             } else {
-                res.status(200).json({ mess: 'Phim này đã có trong danh sách theo dõi!' })
+                res.status(200).json({
+                    mess: 'Phim này đã có trong danh sách theo dõi!',
+                });
                 // let oldEpi = getFullEpi(db.get('follower').find({ title: name }).value().data);
                 // let newEpi = getFullEpi(await refreshUrlData(url))
                 // // console.log(oldEpi)
@@ -66,55 +77,54 @@ module.exports.add = (req, res) => {
                 // }
             }
         } catch (error) {
-            res.json({ mess: 'Url bạn nhập sai hoặc chưa được hỗ trợ!' })
+            res.json({ mess: 'Url bạn nhập sai hoặc chưa được hỗ trợ!' });
         }
     });
-}
+};
 
 module.exports.delete = (req, res) => {
     if (db.get('follower').remove({ title: req.body.title }).write()) {
-        res.sendStatus(200)
+        res.sendStatus(200);
     } else {
-        res.sendStatus(500)
+        res.sendStatus(500);
     }
-}
+};
 
-
-module.exports.adit = (req, res) => {
-
-}
+module.exports.adit = (req, res) => {};
 
 function refreshUrlData(url) {
     return new Promise((resolve, reject) => {
         request(url, (err, res, body) => {
-            if (err) return reject(err)
+            if (err) return reject(err);
             try {
-                const dataFromUrl = body.match(/<li juji-order="(\d+)" class="v-li drama [selected]*">(.*?)<\/li>/mg);
-                const epi = dataFromUrl.map(e => {
+                const dataFromUrl = body.match(
+                    /<li juji-order="(\d+)" class="v-li drama [selected]*">(.*?)<\/li>/gm
+                );
+                const epi = dataFromUrl.map((e) => {
                     const eNumber = e.match(/rseat="(\w+)"/);
                     if (eNumber.input.indexOf('card_vip_icon') > 0) {
                         return {
                             name: eNumber[1],
-                            type: 'Vip'
-                        }
+                            type: 'Vip',
+                        };
                     } else {
                         return {
                             name: eNumber[1],
-                            type: 'Normal'
-                        }
+                            type: 'Normal',
+                        };
                     }
-                })
-                resolve(epi)
+                });
+                resolve(epi);
             } catch (error) {
-                resolve('error')
+                resolve('error');
             }
-        })
-    })
+        });
+    });
 }
 function getFullEpi(epi) {
-    return epi.filter(e => {
+    return epi.filter((e) => {
         if (e.name.indexOf('preview') < 0) {
-            return e
+            return e;
         }
-    })
+    });
 }
--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,63 +1,71 @@
 $(document).ready(function () {
-	// Activate tooltip
-	$('.edit, .delete').on('click', (e) => {
-		let $this = $(e.currentTarget);
-		if ($this.hasClass('delete')) {
-			$('input.btn.btn-danger').on('click', () => {
-				$.post('/delete', {
-					title: $this.attr('data-tl')
-				}, (data, status) => {
-					if (status == 'success') {
-						$($this.parents('tr')).remove()
-					}
-				})
-			})
-		} else if ($this.hasClass('edit')) {
-			$('input[name="name"]').val($this.attr('data-tl'))
-			$('input[name="url"]').val($this.attr('data-url'))
-			$('input[name="time"]').val($this.attr('data-time'))
-		}
-	});
+    // Activate tooltip
+    $('.edit, .delete').on('click', (e) => {
+        let $this = $(e.currentTarget);
+        if ($this.hasClass('delete')) {
+            $('input.btn.btn-danger').on('click', () => {
+                $.post(
+                    '/delete',
+                    {
+                        title: $this.attr('data-tl'),
+                    },
+                    (data, status) => {
+                        if (status == 'success') {
+                            $($this.parents('tr')).remove();
+                        }
+                    }
+                );
+            });
+        } else if ($this.hasClass('edit')) {
+            $('input[name="name"]').val($this.attr('data-tl'));
+            $('input[name="url"]').val($this.attr('data-url'));
+            $('input[name="time"]').val($this.attr('data-time'));
+        }
+    });
 
-	$('#addEmployeeModal input.btn.btn-success').on('click', () => {
-		var url = $('#addEmployeeModal input[name="url"]').val()
-		var time = $('#addEmployeeModal input[name="time"]').val()
-		if (url) {
-			$.post('/add', {
-				url: url,
-				time: time
-			}, (data, status) => {
-				if (status == 'success') {
-					$('tbody').append(data.html);
-					$('.toast-body').text(data.mess);
-					$('.toast').toast('show');
-				} else {
-					$('.toast-body').text(data.mess);
-					$('.toast').toast('show');
-				}
-			})
-		} else {
-			$('.toast-body').text('Url không được để trống!');
-			$('.toast').toast('show');
-		}
-	})
+    $('#addEmployeeModal input.btn.btn-success').on('click', () => {
+        var url = $('#addEmployeeModal input[name="url"]').val();
+        var time = $('#addEmployeeModal input[name="time"]').val();
+        if (url) {
+            $.post(
+                '/add',
+                {
+                    url: url,
+                    time: time,
+                },
+                (data, status) => {
+                    if (status == 'success') {
+                        $('tbody').append(data.html);
+                        $('.toast-body').text(data.mess);
+                        $('.toast').toast('show');
+                    } else {
+                        $('.toast-body').text(data.mess);
+                        $('.toast').toast('show');
+                    }
+                }
+            );
+        } else {
+            $('.toast-body').text('Url không được để trống!');
+            $('.toast').toast('show');
+        }
+    });
 
-	// Select/Deselect checkboxes
-	var checkbox = $('table tbody input[type="checkbox"]');
-	$("#selectAll").click(function () {
-		if (this.checked) {
-			checkbox.each(function () {
-				this.checked = true;
-			});
-		} else {
-			checkbox.each(function () {
-				this.checked = false;
-			});
-		}
-	});
-	checkbox.click(function () {
-		if (!this.checked) {
-			$("#selectAll").prop("checked", false);
-		}
-	});
+    // Select/Deselect checkboxes
+    var checkbox = $('table tbody input[type="checkbox"]');
+    $('#selectAll').click(function () {
+        if (this.checked) {
+            checkbox.each(function () {
+                this.checked = true;
+            });
+        } else {
+            checkbox.each(function () {
+                this.checked = false;
+            });
+        }
+    });
+    checkbox.click(function () {
+        if (!this.checked) {
+            $('#selectAll').prop('checked', false);
+        }
+    });
 });
