27bc5c2cbc4fb776a28044a4703545bdaacf1610
Pencil <pencil.bsp@gmail.com>
2020-08-10 14:27:52 +0700

    fix error cant add new follow

--- controllers/index.js
+++ controllers/index.js
@@ -8,6 +8,7 @@ require('dotenv').config()
 const getData = require('./getData');
 const getTitle = require('./getTitle');
 const getEpi = require('./epi');
+const { time } = require('cron');
 
 db.defaults({ follower: [] }).write();
 
@@ -22,11 +23,11 @@ module.exports.add = async (req, res) => {
     if (namePage == 'www.iq.com') {
         var name = await getTitle.iq(urlRaw);
         var newEpi = getEpi.full(await getData.iq(urlRaw));
-        getIndex(name, newEpi);
+        res.json(getIndex(name, newEpi, urlRaw, req.body.time))
     } else if (namePage == 'fptplay.vn') {
         var name = await getTitle.fptplay(urlRaw);
         var newEpi = await getData.fptplay(urlRaw);
-        getIndex(name, newEpi);
+        res.json(getIndex(name, newEpi, urlRaw, req.body.time));
     } else if (namePage == 'wetv.vip') {
         console.log('wetv.vip')
     } else {
@@ -48,36 +49,36 @@ module.exports.delete = (req, res) => {
 
 module.exports.adit = (req, res) => { };
 
-function getIndex(name, newEpi) {
-    try {
+function getIndex(name, newEpi, url, time) {
+    // try {
         if (db.get('follower').find({ title: name }).value() == undefined) {
             if (newEpi === 'error') {
-                res.json({
+                return {
                     mess: 'Url bạn nhập sai hoặc chưa được hỗ trợ!',
-                });
+                };
             } else {
                 db.get('follower')
                     .push({
                         title: name,
-                        url: urlRaw,
+                        url: url,
                         data: newEpi,
-                        time: req.body.time,
+                        time: time,
                     })
                     .write();
-                const htmlCode = `<tr><td class="name">${name}</td><td class="url"><a href="${urlRaw}" target="_blank">${urlRaw}</a></td><td>${newEpi.length}</td><td>${req.body.time}</td><td><a class="edit" href="#editEmployeeModal" data-tl="${name}" data-url="${urlRaw}" data-toggle="modal"><i class="material-icons" data-toggle="tooltip" title="Edit"></i></a><a class="delete" href="#deleteEmployeeModal" data-tl="${name}" data-toggle="modal"><i class="material-icons" data-toggle="tooltip" title="Delete"></i></a></td></tr>`;
+                const htmlCode = `<tr><td class="name">${name}</td><td class="url"><a href="${url}" target="_blank">${url}</a></td><td>${newEpi.length}</td><td>${time}</td><td><a class="edit" href="#editEmployeeModal" data-tl="${name}" data-url="${url}" data-toggle="modal"><i class="material-icons" data-toggle="tooltip" title="Edit"></i></a><a class="delete" href="#deleteEmployeeModal" data-tl="${name}" data-toggle="modal"><i class="material-icons" data-toggle="tooltip" title="Delete"></i></a></td></tr>`;
                 const messNew = `[${namePage}] Phim ${name} đã được thêm vào danh sách theo dõi với ${newEpi.length} tập mới!`
                 // request(`${process.env.TELEGRAM_URL}${encodeURI(messNew)}`);
-                res.json({
+                return {
                     mess: `Phim ${name} đã được thêm vào danh sách theo dõi!`,
                     html: htmlCode,
-                });
+                };
             }
         } else {
-            res.status(200).json({
+            return {
                 mess: 'Phim này đã có trong danh sách theo dõi!',
-            });
+            };
         }
-    } catch (error) {
-        res.json({ mess: 'Url bạn nhập sai hoặc chưa được hỗ trợ!' });
-    }
+    // } catch (error) {
+    //     return { mess: 'Url bạn nhập sai hoặc chưa được hỗ trợ!' };
+    // }
 }
